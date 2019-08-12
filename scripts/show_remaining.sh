7d8e8da151bc9af4a9d69ab08ebc3038904c93c2
Michael Bright <1880109+mjbright@users.noreply.github.com>
2019-08-12 16:53:33 +0200

    Loop

--- scripts/show_remaining.sh
+++ scripts/show_remaining.sh
@@ -1,1 +1,9 @@
-kubectl get deploy,pods,rs,svc,ValidatingWebhookConfiguration,MutatingWebhookConfiguration | grep -E "webhook|sleep"
+
+
+
+while true; do
+    kubectl get deploy,pods,rs,svc,ValidatingWebhookConfiguration,MutatingWebhookConfiguration | grep -E "webhook|sleep"
+    sleep 1
+    echo "----"
+done
+

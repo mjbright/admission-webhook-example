d856d03b10700e7540b7e13cd16f4af754912e70
Michael Bright <1880109+mjbright@users.noreply.github.com>
2019-08-12 17:31:14 +0200

    Exit loop when nothing to display

--- scripts/show_remaining.sh
+++ scripts/show_remaining.sh
@@ -1,5 +1,16 @@
+#!/bin/bash
 
+#while kubectl get pods | grep "[A-z]" ; do sleep 1; done
 
+while kubectl get deploy,pods,rs,svc,ValidatingWebhookConfiguration,MutatingWebhookConfiguration | grep -E "webhook|sleep"; do
+    sleep 1
+    echo "----"
+done
+
+kubectl get deploy,pods,rs,svc,ValidatingWebhookConfiguration,MutatingWebhookConfiguration | grep -E "webhook|sleep"
+exit 0
+
+#### ---------------------------------------------------------------------
 
 while true; do
     kubectl get deploy,pods,rs,svc,ValidatingWebhookConfiguration,MutatingWebhookConfiguration | grep -E "webhook|sleep"

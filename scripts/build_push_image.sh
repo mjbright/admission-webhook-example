6052258db1d1e986d183747d3c78fa0fadab9f2e
Michael Bright <1880109+mjbright@users.noreply.github.com>
2019-08-12 15:07:40 +0200

    Working 2-stage build

--- scripts/build_push_image.sh
+++ scripts/build_push_image.sh
@@ -6,7 +6,8 @@ TAG="v1"
 IMAGE=${REPO}:${TAG}
 
 echo; echo "---- Building image ${IMAGE} ..."
-docker build -t ${IMAGE} .
+#docker build -t ${IMAGE} .
+docker build -t ${IMAGE} -f Dockerfile.2stage .
 
 echo; echo "---- Pushing  image ${IMAGE} ..."
 docker push ${IMAGE}
